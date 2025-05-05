593a66767bd00fd2968a9ad723ccea115bdb7520
Kartik <1kartiknegi@gmail.com>
2025-05-05 19:14:33 +0530

    Changed methods in Models to static, created logic for train assigment

--- src/main/java/Backend/Addition.java
+++ src/main/java/Backend/Addition.java
@@ -1,4 +1,20 @@
 package Backend;
+import application.Train;
+import application.Platform;
+import Backend.Models;
+import java.time.LocalTime;
 
 public class Addition {
+
+    public static void assignTrain(Train train) {
+        Platform p1 = Models.peekNextPlatform();
+        if (!p1.getNextFree().isAfter(train.getArrivalTime())) {
+            p1 = Models.popNextPlatform();
+            train.setActualArrival(train.getArrivalTime());
+            train.setActualDeparture(train.getDepartureTime());
+            p1.setNextFree(train.getActualDeparture());
+            Models.addPlatform(p1);
+        }
+    }
 }
+
--- src/main/java/Backend/Models.java
+++ src/main/java/Backend/Models.java
@@ -1,4 +1,5 @@
 package Backend;
+
 import java.util.PriorityQueue;
 import java.util.Comparator;
 import application.Platform;
@@ -6,46 +7,41 @@ import application.Train;
 
 public class Models {
 
-    private PriorityQueue<Platform> platformHeap;
-    private PriorityQueue<Train> waitingQueue;
+    private static PriorityQueue<Platform> platformHeap;
+    private static PriorityQueue<Train> waitingQueue;
 
-    public Models() {
+    static {
         platformHeap = new PriorityQueue<>(
                 Comparator.comparing(Platform::getNextFree)
                         .thenComparing(Platform::getId)
         );
-
         waitingQueue = new PriorityQueue<>(
                 Comparator.comparing(Train::getArrivalTime)
                         .thenComparing(Train::getPriority)
         );
     }
 
-    public void addPlatform(Platform platform) {
+    public static void addPlatform(Platform platform) {
         platformHeap.add(platform);
     }
 
-    public Platform popNextPlatform() {
-        // Returns null if the heap is empty
+    public static Platform popNextPlatform() {
         return platformHeap.poll();
     }
 
-    public Platform peekNextPlatform() {
-        // Returns null if the heap is empty
+    public static Platform peekNextPlatform() {
         return platformHeap.peek();
     }
 
-    public void enqueueTrain(Train train) {
+    public static void enqueueTrain(Train train) {
         waitingQueue.add(train);
     }
 
-    public Train dequeueTrain() {
-        // Returns null if the queue is empty
+    public static Train dequeueTrain() {
         return waitingQueue.poll();
     }
 
-    public Train peekWaitingTrain() {
-        // Returns null if the queue is empty
+    public static Train peekWaitingTrain() {
         return waitingQueue.peek();
     }
 }
