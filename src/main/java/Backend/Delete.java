d87a1897b4f317787ebd50ee9bfc3ee399ebf900
Kartik <1kartiknegi@gmail.com>
2025-05-11 16:56:16 +0530

    added platform to delete platfomr logic

--- src/main/java/Backend/Delete.java
+++ src/main/java/Backend/Delete.java
@@ -5,36 +5,39 @@ import application.Platform;
 import java.util.List;
 
 public class Delete {
-    public static void deleteTrain(Train train)
-    {
-        List<Train> tail = Models.dequeueTrainsFromIndex(Models.waitingList.indexOf(train)-1);
+    public static void deleteTrain(Train train) {
+        List<Train> tail = Models.dequeueTrainsFromIndex(Models.waitingList.indexOf(train) - 1);
         Models.tails(tail);
-        for(int i = 2;i<tail.size();++i)
-        {
+        for (int i = 2; i < tail.size(); ++i) {
             Train t = tail.get(i);
             Models.set(t);
         }
         Models.waitingList.addAll(tail);
         Models.waitingList.remove(train);
     }
-    public static int deletePlatform(Platform platform)
-    {
+
+    public static Platform deletePlatform(Platform platform) {
         Train t = null;
-        for(int i = 0;i<Models.processedList.size();++i)
-        {
-           t = Models.processedList.get(i);
-           if(t.getPlatformId()==platform.getId()){
-               platform.setNextFree(t.getActualDeparture());
-               return 1;
-           }
+        Platform p1 = platform;
+        for (int i = 0; i < Models.processedList.size(); ++i) {
+            t = Models.processedList.get(i);
+            if (t.getPlatformId() == platform.getId()) {
+                p1.setNextFree(t.getActualDeparture());
+                Models.platformHeap.remove(platform);
+                Models.tails(Models.waitingList);
+                for (int j = 0; j < Models.waitingList.size(); ++j) {
+                    Train t1 = Models.waitingList.get(j);
+                    Models.set(t1);
+                }
+                return p1;
+            }
         }
             Models.platformHeap.remove(platform);
             Models.tails(Models.waitingList);
-            for(int j = 0;j<Models.waitingList.size();++j)
-            {
+            for (int j = 0; j < Models.waitingList.size(); ++j) {
                 Train t1 = Models.waitingList.get(j);
                 Models.set(t1);
             }
-            return 0;
+            return p1;
+        }
     }
-}
