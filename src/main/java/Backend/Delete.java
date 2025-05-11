bf3d15db847187d98f8a5dd7394710dea51d2eff
Achintya Bist <achintyabist05@gmail.com>
2025-05-11 17:04:49 +0530

    made changes in delete platform

--- src/main/java/Backend/Delete.java
+++ src/main/java/Backend/Delete.java
@@ -2,6 +2,7 @@ package Backend;
 import application.Train;
 import application.Platform;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public class Delete {
@@ -9,8 +10,7 @@ public class Delete {
         List<Train> tail = Models.dequeueTrainsFromIndex(Models.waitingList.indexOf(train) - 1);
         Models.tails(tail);
         for (int i = 2; i < tail.size(); ++i) {
-            Train t = tail.get(i);
-            Models.set(t);
+            Models.set(tail.get(i));
         }
         Models.waitingList.addAll(tail);
         Models.waitingList.remove(train);
@@ -18,26 +18,26 @@ public class Delete {
 
     public static Platform deletePlatform(Platform platform) {
         Train t = null;
-        Platform p1 = platform;
         for (int i = 0; i < Models.processedList.size(); ++i) {
             t = Models.processedList.get(i);
             if (t.getPlatformId() == platform.getId()) {
-                p1.setNextFree(t.getActualDeparture());
+                platform.setNextFree(t.getActualDeparture());
                 Models.platformHeap.remove(platform);
                 Models.tails(Models.waitingList);
                 for (int j = 0; j < Models.waitingList.size(); ++j) {
                     Train t1 = Models.waitingList.get(j);
                     Models.set(t1);
                 }
-                return p1;
+                return platform;
             }
         }
             Models.platformHeap.remove(platform);
+            platform.setNextFree(LocalTime.of(0,0));
             Models.tails(Models.waitingList);
             for (int j = 0; j < Models.waitingList.size(); ++j) {
                 Train t1 = Models.waitingList.get(j);
                 Models.set(t1);
             }
-            return p1;
+            return platform;
         }
     }
